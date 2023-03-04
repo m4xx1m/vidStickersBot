2a51a9db921257f98c4277a42b087e6cf4ff9a91
m4xx1m <m4xx1mz@gmail.com>
2023-03-04 13:52:16 +0300

    move to custom botapi

--- vsbot/misc.py
+++ vsbot/misc.py
@@ -1,12 +1,17 @@
 import json
+
 from vsbot.db import BotDB
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.bot.api import TelegramAPIServer
 
 storage = MemoryStorage()
 db = BotDB()
 with open("volumes/config.json", "r", encoding="utf-8") as _cfg_file:
     config = json.load(_cfg_file)
 
-bot = Bot(token=config["TOKEN"])
+bot = Bot(
+    token=config["TOKEN"],
+    server=TelegramAPIServer.from_base(f"http://botapi:8081/")
+)
 dp = Dispatcher(bot=bot, storage=storage)
